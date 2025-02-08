af426c93bac5046e5390f14fe76e9dc0241fb261
SHUBHAM CHAUHAN <shubhamchauhanpoly@gmail.com>
2025-02-09 04:45:55 +0530

    method and access specifier/modifier check public

---
+++ 12Methods/src/SC/skills/App.java
@@ -0,0 +1,25 @@
+package SC.skills;
+
+public class App {
+    public String str_1 = "I am a public memeber";
+
+    void printFromClass() {
+        System.out.println("within Class: " + str_1);
+    }
+
+    public static void main(String[] args) {
+        App obj = new App();
+        obj.printFromClass();
+        System.out.println("with in class: " + obj.str_1);
+
+        App2 obj2 = new App2();
+        obj2.printOfOutsideClass();
+    }
+}
+
+class App2 {
+void printOfOutsideClass(){
+    App obj = new App();
+    System.out.println("within package , outside class: " + obj.str_1);
+}
+}
