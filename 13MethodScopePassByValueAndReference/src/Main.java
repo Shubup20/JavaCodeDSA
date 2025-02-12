dcd7dc0e91058e7ec1cd834a800c10fc0fe3add4
SHUBHAM CHAUHAN <shubhamchauhanpoly@gmail.com>
2025-02-12 13:43:25 +0530

    Array

--- 13MethodScopePassByValueAndReference/src/Main.java
+++ 13MethodScopePassByValueAndReference/src/Main.java
@@ -18,17 +18,27 @@ class Algebra {
         int a = 10;
         System.out.println(a);
         {
-            int c = 10; //block scope variable
+//            int c = 10; //block scope variable
         }
 //        System.out.println(c);
     }
 }
 
 public class Main {
+ static    void changeValue(int a){
+        a*=100;
+
+     System.out.println("inside change value: "+ a);
+    }
     public static void main(String[] args) {
         Algebra obj = new Algebra();
         System.out.println(obj.add());
         System.out.println(obj.sub());
         obj.demo();
+
+        int a = 10;
+        System.out.println("Before changing value: " + a);
+        changeValue(a);
+        System.out.println("After changing value: " + a);
     }
 }
