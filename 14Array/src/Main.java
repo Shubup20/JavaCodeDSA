5489b053fb89c37fef26f4b4743eb5fe2fa33b97
SHUBHAM CHAUHAN <shubhamchauhanpoly@gmail.com>
2025-02-12 14:37:19 +0530

    sum array

--- 14Array/src/Main.java
+++ 14Array/src/Main.java
@@ -9,6 +9,7 @@ class ArrayExample {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == x) {
             ans = i;
+            break;
             }
             System.out.println("Found " + "x" + ans);
         }
