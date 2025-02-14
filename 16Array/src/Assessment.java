dc08c92dc4319c773f8b33cedca85872a1bd7498
SHUBHAM CHAUHAN <shubhamchauhanpoly@gmail.com>
2025-02-15 00:04:38 +0530

    Assessment for lastRepeating find element

---
+++ 16Array/src/Assessment.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Assessment {
+
+    static int lastRepeatingNumber(int[] arr){
+        int n = arr.length -1;
+        for(int i=n;i>0;i--){
+            for(int j=n-1;j>0;j--){
+                if(arr[i]==arr[j]){
+                    return arr[i];
+                }
+            }
+        }
+        return  -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " element :");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array is : " + Arrays.toString(arr));
+
+        System.out.println("last Repeating element is : " + lastRepeatingNumber(arr));
+
+
+    }
+}
+
